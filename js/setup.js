24dd6946be3e78186379838fbe5ab3432145245e
Vladimir Kolomoyets <kolomoyetsva@gmail.com>
2018-11-21 21:57:49 +0300

    внесены исправления по замечаниям

--- js/setup.js
+++ js/setup.js
@@ -2,33 +2,23 @@
 
 // wizard constructor
 
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
 var wizards = [];
 var wizardsNumber = 4;
 
-var generateWisardName = function () {
-  var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-
-  return NAMES[Math.floor(Math.random() * NAMES.length)] + ' ' + SURNAMES[Math.floor(Math.random() * SURNAMES.length)];
-};
-
-var generateWizardCoatColor = function () {
-  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-
-  return COAT_COLORS[Math.floor(Math.random() * COAT_COLORS.length)];
-};
-
-var generateWizardEyesColor = function () {
-  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-
-  return EYES_COLORS[Math.floor(Math.random() * EYES_COLORS.length)];
+var getRandomElement = function (array) {
+  return array[Math.floor(Math.random() * array.length)];
 };
 
 for (var i = 0; i < wizardsNumber; i++) {
   wizards[i] = {
-    name: generateWisardName(),
-    coatColor: generateWizardCoatColor(),
-    eyesColor: generateWizardEyesColor()
+    name: getRandomElement(NAMES) + ' ' + getRandomElement(SURNAMES),
+    coatColor: getRandomElement(COAT_COLORS),
+    eyesColor: getRandomElement(EYES_COLORS)
   };
 }
 
@@ -58,7 +48,7 @@ var renderWizard = function (wizard) {
 
 // функция заполнения блока DOM-элементами на основе массива JS-объектов
 
-var generateDOMElements = function (array, elementGenerator, positionInDOM) {
+var createDOMElements = function (array, elementGenerator, positionInDOM) {
   var fragment = document.createDocumentFragment();
 
   for (i = 0; i < wizards.length; i++) {
@@ -67,6 +57,6 @@ var generateDOMElements = function (array, elementGenerator, positionInDOM) {
   positionInDOM.appendChild(fragment);
 };
 
-generateDOMElements(wizards, renderWizard, similarListElement);
+createDOMElements(wizards, renderWizard, similarListElement);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
